7e9b4eaf58c84c63015b1c5ffa1060e98d255207
Raoof <devraoof@gmail.com>
2025-09-15 23:08:43 +0530

    Initialize project structure with TanStack, Shadcn, and Biome configurations. Added essential files including package.json, vite.config.ts, and routing setup. Implemented basic components and styles using Tailwind CSS. Included README for project setup instructions.

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import { Link } from "@tanstack/react-router";
+
+export default function Header() {
+	return (
+		<header className="p-2 flex gap-2 bg-white text-black justify-between"></header>
+	);
+}
